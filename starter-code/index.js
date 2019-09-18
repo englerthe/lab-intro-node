dfe9979ebcb269a290b2d69dd2c8edfd2e4ae316
Englert <englert@media-saturn.com>
2019-09-18 15:31:59 +0200

    finished

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,11 +1,48 @@
 class SortedList {
-    constructor() {}
-    add(item) {}
-    get(pos) {}
-    max() {}
-    min() {}
-    average() {}
-    sum() {}
+    constructor() {
+      this.items = [];
+      this.length = 0;
+    }
+    add(item) {
+      this.items.push(item);
+      this.length ++;
+      this.items.sort((a, b) => { return a - b;});
+    }
+    get(pos) {
+      if (pos < this.items.length){
+      return this.items[pos];
+      } else {
+        return;
+      }
+    }
+    max() {
+      if (this.items.length == 0) {
+        throw new Error("EmptySortedList");
+      } else {
+        return this.items[(this.length-1)];
+      }
+    }
+    min() {
+      if (this.items.length == 0) {
+        throw new Error("EmptySortedList");
+      } else {
+        return this.items[0];
+      }
+    }
+    average() {
+      if (this.items.length == 0) {
+        throw new Error("EmptySortedList");
+      } else {
+        return this.items.reduce((a,b) => a + b) / this.items.length;
+      }
+    }
+    sum() {
+      if (this.items.length == 0) {
+        return 0;
+      } else {
+        return this.items.reduce((a,b) => a + b);
+      }
+    }
   };
   
   module.exports = SortedList;
